fb27529ec80cd34f7ec359e32d964916d938d96c
Mamoudou Camara <amos2013.camara02@gmail.com>
2014-11-17 13:32:17 +0100

    Initial commit

---
+++ VS 2010 Demo/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VS_2010_Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Initial commit to  master branch
+        }
+    }
+}
